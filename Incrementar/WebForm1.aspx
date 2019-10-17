1a4cf920bf180b247b73f5e9d1fc4f636af29e39
maquintero451 <maquintero451@misena.edu.co>
2019-10-17 17:12:27 -0500

    app conteo

---
+++ Incrementar/WebForm1.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Incrementar.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            margin-bottom: 0px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="txtNumero" runat="server" CssClass="auto-style1"></asp:TextBox>
+            <asp:Button ID="btnIncrementar" runat="server" OnClick="btnIncrementar_Click" Text="Incrementar" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Incrementar/WebForm1.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Incrementar
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        int i = 0;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                txtNumero.Text = i.ToString();
+            }
+            
+        }
+
+        protected void btnIncrementar_Click(object sender, EventArgs e)
+        {
+            if (ViewState["conteo"] != null)
+            {
+                i = (int)ViewState["conteo"] + 1;
+                
+                
+            }
+            txtNumero.Text = i.ToString();
+            ViewState["conteo"] = i;
+        }
+    }
+}
